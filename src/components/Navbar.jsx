ab6c7f0ececedc9e70d27e2b4e6c3025b7bee1df
Kavita539 <67622582+Kavita539@users.noreply.github.com>
2022-08-24 20:05:18 +0530

    feat: Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+return(
+          <nav className="nav bg-blue-500 text-slate-50 p-4 sticky top-0 right-0 left-0 shadow-md">
+              <header>
+                  <h1 className="text-2xl font-bold">Flipkart</h1>
+              </header>
+          </nav>
+);
+}
+
+export { Navbar };
